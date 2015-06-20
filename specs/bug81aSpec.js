d64524bc71213f00e8231663069900058016f721
Luis Rudge <luis@luisrudge.net>
2015-06-19 23:47:53 -0300

    refactoring bug81aspec

--- specs/bug81aSpec.js
+++ specs/bug81aSpec.js
@@ -3,33 +3,48 @@ var test = require('./test');
 describe('bug 8.1.a', function() {
     describe('expands shorthand', function() {
         it('with px', function(done) {
-            test('a{flex: 1 0 calc(1px);}', 'a{flex-grow: 1;flex-shrink: 0;flex-basis: calc(1px);}', {}, done);
+            var input = 'a{flex: 1 0 calc(1px);}';
+            var output = 'a{flex-grow: 1;flex-shrink: 0;flex-basis: calc(1px);}';
+            test(input, output, {}, done);
         });
         it('with px-px', function(done) {
-            test('a{flex: 0 1 calc(1px - 1px);}', 'a{flex-grow: 0;flex-shrink: 1;flex-basis: calc(1px - 1px);}', {}, done);
+            var input = 'a{flex: 0 1 calc(1px - 1px);}';
+            var output = 'a{flex-grow: 0;flex-shrink: 1;flex-basis: calc(1px - 1px);}';
+            test(input, output, {}, done);
         });
         it('with vw', function(done) {
-            test('a{flex: 1 0 calc(1vw);}', 'a{flex-grow: 1;flex-shrink: 0;flex-basis: calc(1vw);}', {}, done);
+            var input = 'a{flex: 1 0 calc(1vw);}';
+            var output = 'a{flex-grow: 1;flex-shrink: 0;flex-basis: calc(1vw);}';
+            test(input, output, {}, done);
         });
         it('with vw-vw', function(done) {
-            test('a{flex: 0 1 calc(1vw-1vw);}', 'a{flex-grow: 0;flex-shrink: 1;flex-basis: calc(1vw-1vw);}', {}, done);
+            var input = 'a{flex: 0 1 calc(1vw-1vw);}';
+            var output = 'a{flex-grow: 0;flex-shrink: 1;flex-basis: calc(1vw-1vw);}';
+            test(input, output, {}, done);
         });
         it('with vw-px', function(done) {
-            test('a{flex: 1 0 calc(1vw-1px);}', 'a{flex-grow: 1;flex-shrink: 0;flex-basis: calc(1vw-1px);}', {}, done);
+            var input = 'a{flex: 1 0 calc(1vw-1px);}';
+            var output = 'a{flex-grow: 1;flex-shrink: 0;flex-basis: calc(1vw-1px);}';
+            test(input, output, {}, done);
         });
         it('with %', function(done) {
-            test('a{flex: 0 1 calc(1vw);}', 'a{flex-grow: 0;flex-shrink: 1;flex-basis: calc(1vw);}', {}, done);
+            var input = 'a{flex: 0 1 calc(1vw);}';
+            var output = 'a{flex-grow: 0;flex-shrink: 1;flex-basis: calc(1vw);}';
+            test(input, output, {}, done);
         });
     });
     describe('does nothing', function() {
         it('when using only first value', function(done) {
-            test('a{flex: 0}', 'a{flex: 0}', {}, done);
+            var css = 'a{flex: 0}';
+            test(css, css, {}, done);
         });
         it('when using only first and second values', function(done) {
-            test('a{flex: 0 0}', 'a{flex: 0 0}', {}, done);
+            var css = 'a{flex: 0 0}';
+            test(css, css, {}, done);
         });
         it('when not using calc', function(done) {
-            test('a{flex: 0 0 1px}', 'a{flex: 0 0 1px}', {}, done);
+            var css = 'a{flex: 0 0 1px}';
+            test(css, css, {}, done);
         });
     });
 });
