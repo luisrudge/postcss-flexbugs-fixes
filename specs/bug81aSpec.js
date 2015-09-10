5932d3685c38ea969eee9148959d8cba320d06e6
Matija Marohnić <matija.marohnic@gmail.com>
2015-09-10 18:41:51 +0200

    Remove unnecessary tests
    The regex `/calc\(.*?\)/` safely covers any argument to `calc()`.

--- specs/bug81aSpec.js
+++ specs/bug81aSpec.js
@@ -1,37 +1,10 @@
 var test = require('./test');
 
 describe('bug 8.1.a', function() {
-    describe('expands shorthand', function() {
-        it('with px', function(done) {
-            var input = 'a{flex: 1 0 calc(1px);}';
-            var output = 'a{flex-grow: 1;flex-shrink: 0;flex-basis: calc(1px);}';
-            test(input, output, {}, done);
-        });
-        it('with px-px', function(done) {
-            var input = 'a{flex: 0 1 calc(1px - 1px);}';
-            var output = 'a{flex-grow: 0;flex-shrink: 1;flex-basis: calc(1px - 1px);}';
-            test(input, output, {}, done);
-        });
-        it('with vw', function(done) {
-            var input = 'a{flex: 1 0 calc(1vw);}';
-            var output = 'a{flex-grow: 1;flex-shrink: 0;flex-basis: calc(1vw);}';
-            test(input, output, {}, done);
-        });
-        it('with vw-vw', function(done) {
-            var input = 'a{flex: 0 1 calc(1vw-1vw);}';
-            var output = 'a{flex-grow: 0;flex-shrink: 1;flex-basis: calc(1vw-1vw);}';
-            test(input, output, {}, done);
-        });
-        it('with vw-px', function(done) {
-            var input = 'a{flex: 1 0 calc(1vw-1px);}';
-            var output = 'a{flex-grow: 1;flex-shrink: 0;flex-basis: calc(1vw-1px);}';
-            test(input, output, {}, done);
-        });
-        it('with %', function(done) {
-            var input = 'a{flex: 0 1 calc(1vw);}';
-            var output = 'a{flex-grow: 0;flex-shrink: 1;flex-basis: calc(1vw);}';
-            test(input, output, {}, done);
-        });
+    it('Expands the shorthand when calc() is used', function(done) {
+        var input = 'a{flex: 1 0 calc(1vw - 1px);}';
+        var output = 'a{flex-grow: 1;flex-shrink: 0;flex-basis: calc(1vw - 1px);}';
+        test(input, output, {}, done);
     });
     describe('does nothing', function() {
         it('when using only first value', function(done) {
