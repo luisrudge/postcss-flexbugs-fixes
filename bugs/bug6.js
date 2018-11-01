d7e62481df7288f1c95840363042e62a29bea579
Arturs Jansons <a.jansons@gmail.com>
2018-11-01 14:09:38 +0300

    Fix flexBasis default value and 0 value normalization

--- bugs/bug6.js
+++ bugs/bug6.js
@@ -7,9 +7,8 @@ module.exports = function(decl) {
         var flexShrink = values[1] || '1';
         var flexBasis = values[2] || 'auto';
         // Safari seems to hate '0%' and the others seems to make do with a nice value when basis is missing,
-        // so if we see a '0%', just remove it.  This way it'll get adjusted for any other cases where '0%' is
-        // already defined somewhere else.
-        if (flexBasis === '0%' || flexBasis === '0px') flexBasis = 'auto';
-        decl.value = flexGrow + ' ' + flexShrink + (flexBasis ? ' ' + flexBasis : '');
+        // so if we see a '0%' or '0px', normalize it to '0'.
+        if (flexBasis === '0%' || flexBasis === '0px') flexBasis = '0';
+        decl.value = flexGrow + ' ' + flexShrink + ' ' + flexBasis;
     }
 };
