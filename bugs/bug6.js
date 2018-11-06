7e5ae3e7823612e6d57a625e6e3734d3766bd18d
Arturs Jansons <a.jansons@gmail.com>
2018-11-06 16:40:56 +0300

    Revert to '0%'

--- bugs/bug6.js
+++ bugs/bug6.js
@@ -11,8 +11,9 @@ module.exports = function(decl) {
         var flexShrink = values[1] || '1';
         var flexBasis = values[2] || '0%';
         // Safari seems to hate '0%' and the others seems to make do with a nice value when basis is missing,
-        // so if we see a '0%' or '0px', normalize it to '0'.
-        if (flexBasis === '0' || flexBasis === '0%' ||flexBasis === '0px') flexBasis = '0';
-        decl.value = flexGrow + ' ' + flexShrink + ' ' + flexBasis;
+        // so if we see a '0%', just remove it.  This way it'll get adjusted for any other cases where '0%' is
+        // already defined somewhere else.
+        if(flexBasis === '0%') flexBasis = null;
+        decl.value = flexGrow + ' ' + flexShrink + (flexBasis ? ' ' + flexBasis : '');
     }
 };
