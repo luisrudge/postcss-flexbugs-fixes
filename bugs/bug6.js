68f6c86bc0b8f769f79be43ca8f0290b5215abd5
Arturs Jansons <a.jansons@gmail.com>
2018-11-06 17:58:01 +0300

    Update bug6.js

--- bugs/bug6.js
+++ bugs/bug6.js
@@ -2,14 +2,14 @@ var postcss = require('postcss');
 
 module.exports = function(decl) {
     if (decl.prop === 'flex') {
-        if (decl.value === 'initial') {
-            decl.value = '0 1 auto';
-            return;
-        }
         var values = postcss.list.space(decl.value);
         var flexGrow = values[0];
         var flexShrink = values[1] || '1';
         var flexBasis = values[2] || '0%';
+        if (flexGrow === 'initial') {
+            decl.value = '0 1 auto';
+            return;
+        }
         // Safari seems to hate '0%' and the others seems to make do with a nice value when basis is missing,
         // so if we see a '0%', just remove it.  This way it'll get adjusted for any other cases where '0%' is
         // already defined somewhere else.
